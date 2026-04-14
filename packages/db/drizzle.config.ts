221bc0fdf2ef536fad20ebbce1d31d3e09bfa8cb
Keita Ito <keitaito@keitaito.net>
2026-04-14 23:01:12 +0900

    feat: add tsconfig

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: "./schema",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env["DATABASE_URL"] as string,
   },
   verbose: true,
 });
