8a1fd483946e5602748c8618741377e14942da1e
Keita Ito <keitaito@keitaito.net>
2026-04-15 19:22:42 +0900

    feat: install close-with-grace

--- packages/private-api/src/main.ts
+++ packages/private-api/src/main.ts
@@ -1,20 +1,24 @@
-import { createServer } from "./create-server";
+import closeWithGrace from "close-with-grace";
 import { v7 as uuid } from "uuid";
+import { createServer } from "./create-server";
 
 async function main() {
   const server = await createServer({
     logger: true,
-    genReqId() {
-      return uuid();
-    },
+    genReqId: () => uuid(),
   });
 
-  server.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
+  closeWithGrace({ delay: 500 }, async ({ err }) => {
+    if (err) server.log.error({ err }, "shutdown error");
+    await server.close();
   });
+
+  try {
+    await server.listen({ port: 3000, host: "0.0.0.0" });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 }
 
 main();
