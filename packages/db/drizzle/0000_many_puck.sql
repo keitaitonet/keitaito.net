1f78c940d3c8e6fa4b3b28654d79294680847d28
Keita Ito <keitaito@keitaito.net>
2026-04-20 19:31:17 +0900

    chore: delete project

--- packages/db/drizzle/0000_many_puck.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "activities" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"title" text NOT NULL,
-	"description" text NOT NULL,
-	"date" date NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
