d5be9caa834c71a563c123d93c34423dd3abfa9e
Keita Ito <keitaito@keitaito.net>
2026-04-22 17:16:07 +0900

    feat: image constrained

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://keitaito.net",
+  image: {
+    layout: "constrained",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
